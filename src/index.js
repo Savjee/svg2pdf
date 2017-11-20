41954ff83ffa0c4a7ef55a7be4b800e40302b0ee
Xavier Decuyper <hi@savjee.be>
2017-11-20 13:56:23 +0100

    Print clear line to console before starting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+#!/usr/bin/env node
+
 const utils = require('./utils/index.js');
 
-// 1. Set up a default settings object (-> could be places in separate file)
+// Empty line
+console.log();
+
+// 1. Set up a default settings object
 const config = utils.defaultConfig;
 
 // 2. Parse the CLI options with Commander, populate the config
@@ -8,7 +13,6 @@ utils.commandParser(config);
 
 // 3. Run the preflight checks to make sure everything is in order!
 utils.preflightChecks(config);
-
 
 // 4. Give the config to svg2pdf main code
 utils.svg2pdf(config, () => {
