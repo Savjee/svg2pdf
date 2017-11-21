5293ea7f6a76d453eba6de30186a12b74938cd11
Xavier Decuyper <hi@savjee.be>
2017-11-21 08:26:45 +0100

    Unit tests: using BeforeEach

--- test/test.preflightChecks.js
+++ test/test.preflightChecks.js
@@ -2,10 +2,15 @@ const expect = require('chai').expect;
 const utils = require('../src/utils/index.js');
 
 describe('Testing preflight checks', () => {
+  let config;
+
+  // Make a new copy of the config JSON before running each test
+  beforeEach(function() {
+    config = JSON.parse(JSON.stringify(utils.defaultConfig));
+  });
 
   it('should fail when path to inkscape is invalid', () => {
-    const config = getCopyOfDefaultSettings();
-    config.pathToInkscape = "/some/random/stuff/that/will/never/exist";
+    config.pathToInkscape = '/some/random/stuff/that/will/never/exist';
 
     expect(() => {
       utils.preflightChecks(config);
@@ -13,8 +18,7 @@ describe('Testing preflight checks', () => {
   });
 
   it('should fail when input directory is invalid', () => {
-    const config = getCopyOfDefaultSettings();
-    config.inputDirectory = "/some/random/stuff/that/will/never/exist";
+    config.inputDirectory = '/some/random/stuff/that/will/never/exist';
 
     expect(() => {
       utils.preflightChecks(config);
@@ -22,8 +26,7 @@ describe('Testing preflight checks', () => {
   });
 
   it('should fail when output directory is invalid', () => {
-    const config = getCopyOfDefaultSettings();
-    config.outputDirectory = "/some/random/stuff/that/will/never/exist";
+    config.outputDirectory = '/some/random/stuff/that/will/never/exist';
 
     expect(() => {
       utils.preflightChecks(config);
@@ -31,19 +34,11 @@ describe('Testing preflight checks', () => {
   });
 
   it('should work when input & output directory is defined', () => {
-    const config = getCopyOfDefaultSettings();
-    config.outputDirectory = __dirname + "/output";
-    config.inputDirectory = __dirname + "/input";
+    config.outputDirectory = __dirname + '/output';
+    config.inputDirectory = __dirname + '/input';
 
     expect(() => {
       utils.preflightChecks(config);
     }).to.not.throw();
-
   });
-
 });
-
-function getCopyOfDefaultSettings() {
-  const config = utils.defaultConfig;
-  return JSON.parse(JSON.stringify(config));
-}
